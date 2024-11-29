ea523ce2bcbbdd552b6fcc1fa35b99106dbf0ee1
Romasav <roma.savitski@gmail.com>
2024-11-29 16:41:50 +0000

    add providers

---
+++ lib/domain/providers/get_settings_use_case.g.dart
@@ -0,0 +1,28 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'get_settings_use_case.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$saveSettingsHash() => r'87430bfe6ae3d39b7479be10c59993a4ca060b58';
+
+/// Get settings provider for the use case [GetSettings]
+///
+/// Copied from [saveSettings].
+@ProviderFor(saveSettings)
+final saveSettingsProvider = AutoDisposeProvider<GetSettings>.internal(
+  saveSettings,
+  name: r'saveSettingsProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$saveSettingsHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef SaveSettingsRef = AutoDisposeProviderRef<GetSettings>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
---
+++ lib/domain/providers/save_settings_use_case.g.dart
@@ -0,0 +1,28 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'save_settings_use_case.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$saveSettingsHash() => r'21f4430b2eb1214067e2250fa155c43da4b152ec';
+
+/// Save settings provider for the use case [SaveSettings]
+///
+/// Copied from [saveSettings].
+@ProviderFor(saveSettings)
+final saveSettingsProvider = AutoDisposeProvider<SaveSettings>.internal(
+  saveSettings,
+  name: r'saveSettingsProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$saveSettingsHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef SaveSettingsRef = AutoDisposeProviderRef<SaveSettings>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
