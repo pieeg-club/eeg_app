dba6b0e7012bc8c07b87b1e3ae12661ffbfed149
Romasav <roma.savitski@gmail.com>
2024-12-18 17:12:00 +0000

    integrate data storage into data stream

--- lib/domain/use_cases/get_processed_data_stream_use_case.dart
+++ lib/domain/use_cases/get_processed_data_stream_use_case.dart
@@ -3,16 +3,22 @@ import 'package:eeg_app/core/failure.dart';
 import 'package:eeg_app/core/use_case.dart';
 import 'package:eeg_app/domain/algorithms/algorithm.dart';
 import 'package:eeg_app/domain/entities/algorithm_results/algorithm_result.dart';
+import 'package:eeg_app/domain/repositories/data_storage_repo.dart';
 import 'package:eeg_app/domain/repositories/device_repo.dart';
 
 /// A use case that gets a stream of processed data.
 class GetProcessedDataStreamUseCase
     implements UseCase<Stream<Either<Failure, AlgorithmResult>>, NoParams> {
-  /// Constructs a [GetProcessedDataStreamUseCase] with the given [DeviceRepo].
-  GetProcessedDataStreamUseCase(this._deviceRepo, this._algorithm);
+  /// Constructor for the use case.
+  GetProcessedDataStreamUseCase(
+    this._deviceRepo,
+    this._algorithm,
+    this._dataStorageRepo,
+  );
 
   final DeviceRepo _deviceRepo;
   final Algorithm _algorithm;
+  final DataStorageRepo _dataStorageRepo;
 
   @override
   Future<Either<Failure, Stream<Either<Failure, AlgorithmResult>>>> call(
@@ -34,6 +40,7 @@ class GetProcessedDataStreamUseCase
     Stream<List<int>> dataStream,
   ) async* {
     await for (final rawData in dataStream) {
+      await _dataStorageRepo.saveData(rawData.toString());
       final eitherResult = await _algorithm(rawData);
 
       // Using `yield*` here allows the folded streams to produce
