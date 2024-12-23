5f2144c2c2a83275d5dd2841aad8b2d7b6d1401a
Romasav <roma.savitski@gmail.com>
2024-12-23 16:56:50 +0000

    update band pass to react to settings changes

--- lib/data/repositories/processing_steps/band_pass_step.dart
+++ lib/data/repositories/processing_steps/band_pass_step.dart
@@ -3,6 +3,7 @@ import 'package:eeg_app/domain/failures/processing_step_failures.dart';
 import 'package:eeg_app/domain/repositories/processing_step_repo.dart';
 import 'package:eeg_app/domain/repositories/settings_repo.dart';
 import 'package:iirjdart/butterworth.dart';
+import 'package:synchronized/synchronized.dart';
 
 const double _samplingFrequency = 250;
 const int _order = 5;
@@ -11,40 +12,50 @@ const int _order = 5;
 class BandPassStep
     implements ProcessingStepRepo<List<List<double>>, List<List<double>>> {
   /// Constructor for the band pass filter
-  BandPassStep(this._settings)
-      : _butterworths = List.generate(4, (_) => Butterworth()) {
+  BandPassStep(this._settings) {
     _initializeBandPassFilters();
+
+    _settings.getSettingsStream().listen((event) {
+      _initializeBandPassFilters();
+    });
   }
 
-  final List<Butterworth> _butterworths;
+  late List<Butterworth> _butterworths;
   final SettingsRepo _settings;
+  final _lock = Lock();
 
   @override
   Future<Either<ProcessingStepFailure, List<List<double>>>> call(
     List<List<double>> input,
   ) async {
-    final output = List<List<double>>.generate(
-      input.length,
-      (_) => List<double>.filled(input[0].length, 0),
-    );
-    for (var channel = 0; channel < input.length; channel++) {
-      for (var i = 0; i < input[channel].length; i++) {
-        output[channel][i] = _butterworths[channel].filter(input[channel][i]);
+    return _lock.synchronized(() async {
+      final output = List<List<double>>.generate(
+        input.length,
+        (_) => List<double>.filled(input[0].length, 0),
+      );
+      for (var channel = 0; channel < input.length; channel++) {
+        for (var i = 0; i < input[channel].length; i++) {
+          output[channel][i] = _butterworths[channel].filter(input[channel][i]);
+        }
       }
-    }
-    return Right(output);
+      return Right(output);
+    });
   }
 
   Future<void> _initializeBandPassFilters() async {
-    final settings = await _settings.getSettings();
-    var leftCutOffFreq = 0.0;
-    var rightCutOffFreq = 0.0;
-    leftCutOffFreq = settings.bandPassLowCutOff;
-    rightCutOffFreq = settings.bandPassHighCutOff;
-    final centerFreq = (rightCutOffFreq + leftCutOffFreq) / 2;
-    final widthInFreq = rightCutOffFreq - leftCutOffFreq;
-    for (final filter in _butterworths) {
-      filter.bandPass(_order, _samplingFrequency, centerFreq, widthInFreq);
-    }
+    await _lock.synchronized(() async {
+      final settings = await _settings.getSettings();
+      _butterworths = List<Butterworth>.generate(
+        4,
+        (_) => Butterworth(),
+      );
+      final leftCutOffFreq = settings.bandPassLowCutOff;
+      final rightCutOffFreq = settings.bandPassHighCutOff;
+      final centerFreq = (rightCutOffFreq + leftCutOffFreq) / 2;
+      final widthInFreq = rightCutOffFreq - leftCutOffFreq;
+      for (final filter in _butterworths) {
+        filter.bandPass(_order, _samplingFrequency, centerFreq, widthInFreq);
+      }
+    });
   }
 }
