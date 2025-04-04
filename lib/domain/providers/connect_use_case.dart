3b3a453b49c87c0431dffa51fbf88a57492f6818
Romasav <roma.savitski@gmail.com>
2025-04-04 15:51:34 +0100

    add success and failed connection to log

--- lib/domain/providers/connect_use_case.dart
+++ lib/domain/providers/connect_use_case.dart
@@ -1,6 +1,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:eeg_app/core/use_case.dart';
 import 'package:eeg_app/data/providers/device_repo_impl_provider.dart';
+import 'package:eeg_app/data/providers/log_repo_impl_provider.dart';
 import 'package:eeg_app/domain/use_cases/connect_use_case.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
@@ -11,5 +12,6 @@ part 'connect_use_case.g.dart';
 @riverpod
 UseCase<Unit, NoParams> connectUseCase(Ref ref) {
   final deviceRepo = ref.read(deviceRepoProvider);
-  return ConnectUseCase(deviceRepo);
+  final logRepo = ref.read(logRepoProvider);
+  return ConnectUseCase(deviceRepo, logRepo);
 }
--- lib/domain/use_cases/connect_use_case.dart
+++ lib/domain/use_cases/connect_use_case.dart
@@ -2,16 +2,24 @@ import 'package:dartz/dartz.dart';
 import 'package:eeg_app/core/failure.dart';
 import 'package:eeg_app/core/use_case.dart';
 import 'package:eeg_app/domain/repositories/device_repo.dart';
+import 'package:eeg_app/domain/repositories/log_repo.dart';
 
 /// A use case that connects to the BLE device.
 class ConnectUseCase implements UseCase<Unit, NoParams> {
   /// Constructs a [ConnectUseCase] with the given [DeviceRepo].
-  ConnectUseCase(this._deviceRepo);
+  ConnectUseCase(this._deviceRepo, this._logRepo);
 
   final DeviceRepo _deviceRepo;
+  final LogRepo _logRepo;
 
   @override
-  Future<Either<Failure, Unit>> call(NoParams params) {
-    return _deviceRepo.connect();
+  Future<Either<Failure, Unit>> call(NoParams params) async {
+    final result = await _deviceRepo.connect();
+    result.fold((failure) {
+      _logRepo.logError(failure.message);
+    }, (data) {
+      _logRepo.logInfo('Connected to device');
+    });
+    return result;
   }
 }
